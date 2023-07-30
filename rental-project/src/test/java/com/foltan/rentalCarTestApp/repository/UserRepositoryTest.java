80edd94f0869c056b45141bae1ae7cc8f979341b
AndrejF6512 <89728570+AndrejF6512@users.noreply.github.com>
2023-07-30 20:04:49 +0200

    Update UserRepositoryTest.java

--- rental-project/src/test/java/com/foltan/rentalCarTestApp/repository/UserRepositoryTest.java
+++ rental-project/src/test/java/com/foltan/rentalCarTestApp/repository/UserRepositoryTest.java
@@ -21,10 +21,10 @@ class UserRepositoryTest {
 
         @BeforeEach
         void setUp() {
-                User user = new User(null, "Lawrence", "Okolie", "lawrence123", "apples678", "lpf56@gmail.com", 888777621, null, null, new ArrayList<>());
-                User user2 = new User(null, "Anthony", "Joshua", "joshua456", "eddiehearn678", "gbjosh@onet.pl", 465999222, null, null, new ArrayList<>());
-                User user3 = new User(null, "Tyson", "Fury", "gypsyKing", "deeoonthai", "gypsyKing655@gmail.com", 765555444, null, null, new ArrayList<>());
-                User user4 = new User(null, "Saul", "Alvarez", "canelo", "prettyBoy", "Goose@gmail.com", 908764220, null, null, new ArrayList<>());
+                User user = new User(null, "Andrej", "Foltán", "Foltan123", "foltan", "foltana@gmail.com", 888777621, null, null, new ArrayList<>());
+                User user2 = new User(null, "Peter", "Pevný", "Pevny456", "pevny", "pevnyp@gmail.com", 465999222, null, null, new ArrayList<>());
+                User user3 = new User(null, "Karol", "Malý", "Maly567", "maly", "malyk@gmail.com", 765555444, null, null, new ArrayList<>());
+                User user4 = new User(null, "Petra", "Veselá", "Petra963", "vesela", "veselap@gmail.com", 908764220, null, null, new ArrayList<>());
                 userRepository.save(user);
                 userRepository.save(user2);
                 userRepository.save(user3);
@@ -38,12 +38,12 @@ class UserRepositoryTest {
 
         @Test
         void itShouldReturnTrueIfUsernameExists() {
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("lawrence123")).isInstanceOf(Optional.class).isPresent();
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Gregory56")).isInstanceOf(Optional.class).isEmpty();
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("gypsyKing")).isInstanceOf(Optional.class).isPresent();
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("joshua456")).isInstanceOf(Optional.class).isPresent();
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("programmer777")).isInstanceOf(Optional.class).isEmpty();
-                AssertionsForClassTypes.assertThat(userRepository.findByUsername("canelo")).isInstanceOf(Optional.class).isPresent();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Foltan123")).isInstanceOf(Optional.class).isPresent();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Gregor56")).isInstanceOf(Optional.class).isEmpty();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Maly567")).isInstanceOf(Optional.class).isPresent();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Pevny456")).isInstanceOf(Optional.class).isPresent();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Jozef777")).isInstanceOf(Optional.class).isEmpty();
+                AssertionsForClassTypes.assertThat(userRepository.findByUsername("Petra963")).isInstanceOf(Optional.class).isPresent();
 
         }
 
